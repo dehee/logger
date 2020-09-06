8f3464e292fd17f5720de850deeb69a90472613a
dehee <email@dennisheer.de>
2020-09-06 22:07:57 +0200

    changes

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?
 if ($_GET["id"] != ""){
 	$_GET["d"] = date("Y-m-d H:i:s");
-	print_r($_GET);
+	//print_r($_GET);
 	$myfile = fopen("logfile.txt", "a");
-	fwrite($myfile, "\n" & json_encode($_GET, 1));
+	$jsonText = json_encode($_GET, 1);
+	print_r($jsonText);
+	fwrite($myfile, "\n" & $jsonText);
 	fclose($myfile);
 	
 } 
