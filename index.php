41777c352f346491192d7646ef834deaaf9f133e
dehee <email@dennisheer.de>
2020-09-06 21:56:28 +0200

    write data to file

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 if ($_GET["id"] != ""){
 	print_r($_GET);
 	$myfile = fopen("logfile.txt", "a");
-	$jsonText = "{"\"id\": \""+$_GET("id")+"\", \"t\": \""+$_GET("t")+"\", \"h\": \""+$_GET("h")+"\", \"d\": \""+date("Y-m-d h:i:sa")+"\"}";
+	$jsonText = "{\"id\": \""+$_GET("id")+"\", \"t\": \""+$_GET("t")+"\", \"h\": \""+$_GET("h")+"\", \"d\": \""+date("Y-m-d h:i:sa")+"\"}";
 	fwrite($myfile, "\n" + $jsonText);
 	fclose($myfile);
 	
-}
+} 
 ?>
 <html>
 <body>
