edcf1fcf1620a10fd48f5fa4c5895cc1f48106b3
dehee <email@dennisheer.de>
2020-09-06 21:44:52 +0200

    write data to file

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?
+if ($_GET["id"] != ""){
+	$myfile = fopen("logfile.txt", "a");
+	$jsonText = "{"
+				+"\"id\": \""+$_GET("id")+"\""
+				+"\"t\": \""+$_GET("t")+"\""
+				+"\"h\": \""+$_GET("h")+"\""
+				+"\"d\": \""+date("Y-m-d h:i:sa")+"\""
+				+"}";
+	fwrite($myfile, "\n" + $jsonText);
+	fclose($myfile);
+	
+}
+?>
 <html>
 <body>
 Hello World
